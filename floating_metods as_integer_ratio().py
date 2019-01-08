e5dff66cf2a0e51d937956a4db6cc8de9ef0f5ff
gokayozcoban <43710003+gokayozcoban@users.noreply.github.com>
2019-01-08 22:49:24 +0200

    floating_metods as_integer_ratio()

---
+++ floating_metods as_integer_ratio().py
@@ -0,0 +1,8 @@
+# floating_metods as_integer_ratio()
+# BU METOD ELDEKİ VERİNİN KAÇA BÖLÜNDÜĞÜNDE ELDE EDİLDİĞİNİ GÖSTERİR.
+bu_sayı_kaça_bölünürse_bu_sayı_olur = 4.5
+print(bu_sayı_kaça_bölünürse_bu_sayı_olur.as_integer_ratio())
+
+
+ben_daha_kaça_bölüneyim_a_dostlar = 5.8
+print(ben_daha_kaça_bölüneyim_a_dostlar.as_integer_ratio())
